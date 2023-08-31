969a65526cc996942d6f482c26cab00b1447f5f2
johnechono <johninalegwu22@gmail.com>
2023-08-31 05:28:05 -0700

    Heap - Extract

---
+++ 133-heap_extract.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+* heap_extract - function that extracts the root node
+* @root: a double pointer to the root node of heap
+* Return: a value stored in the root node
+*/
+
+int heap_extract(heap_t **root)
+{
+	printf("%d", (*root)->n);
+	return (0);
+}
